8f4096a6b05e43c5a8404b69c83200b454f0373a
Amit Yoffe <yoffe.amit@gmail.com>
2025-01-02 17:47:21 +0200

    added styling to select field as well

--- src/app/(main)/books/components/drawer/editComponents/styled.tsx
+++ src/app/(main)/books/components/drawer/editComponents/styled.tsx
@@ -2,6 +2,13 @@ import { Box, styled, TextField } from "@mui/material";
 
 export const StyledSelect = styled(TextField)(({ theme }) => ({
   backgroundColor: theme.palette.secondary.main,
+  "& label": {
+    transformOrigin: "right !important",
+    left: "inherit !important",
+    right: "1.75rem !important",
+    fontSize: "small",
+    backgroundColor: theme.palette.primary.light,
+  },
 }));
 
 export const StyledFieldsContainer = styled(Box)(() => ({
